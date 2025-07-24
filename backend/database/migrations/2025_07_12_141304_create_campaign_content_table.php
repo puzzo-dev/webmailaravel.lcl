f03ceb2464bc99ddd87c749a0f762d3b6f2f4c4c
Codie Gambino <og.puzzo@gmail.com>
2025-07-24 10:56:29 +0000

    Important Changes

--- backend/database/migrations/2025_07_12_141304_create_campaign_content_table.php
+++ backend/database/migrations/2025_07_12_141304_create_campaign_content_table.php
@@ -1,31 +1,31 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('campaign_content', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('campaign_id')->constrained()->onDelete('cascade');
-            $table->foreignId('content_id')->constrained()->onDelete('cascade');
-            $table->timestamps();
+// return new class extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      */
+//     public function up(): void
+//     {
+//         Schema::create('campaign_content', function (Blueprint $table) {
+//             $table->id();
+//             $table->foreignId('campaign_id')->constrained()->onDelete('cascade');
+//             $table->foreignId('content_id')->constrained()->onDelete('cascade');
+//             $table->timestamps();
 
-            $table->unique(['campaign_id', 'content_id']);
-        });
-    }
+//             $table->unique(['campaign_id', 'content_id']);
+//         });
+//     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('campaign_content');
-    }
+//     /**
+//      * Reverse the migrations.
+//      */
+//     public function down(): void
+//     {
+//         Schema::dropIfExists('campaign_content');
+//     }
 };
